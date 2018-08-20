64a5b09fc95c722a8a3cde6212b12919d219d708
djpluki <djpluki@gmail.com>
2018-08-20 22:08:58 +0200

    trial for broadcasting

--- server/server.js
+++ server/server.js
@@ -14,19 +14,40 @@ app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("new user connected");
 
+  // for single connection (so, only for sender)
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  // for all except sender
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("createMessage", message => {
     console.log("createMessage showed on server", message);
 
+    //   //for all connections
     io.emit("newMessage", {
-      //for all connections
       from: message.from,
       text: message.text,
       createAt: new Date().getTime()
     });
+
+    // to everyone, except sender
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
   });
 
+  //   // for single connection (so, only for sender)
   // socket.emit("newMessage", {
-  //   // for single connection
   //   from: "John",
   //   text: "I am sent from server",
   //   createdAt: 123
